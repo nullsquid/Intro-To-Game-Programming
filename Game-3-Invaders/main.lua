fd198a71be476033fe4964e38501c9e32f4bcf09
Hyacinth Nil <hyacinth@hyacinthnil.com>
2020-04-01 15:08:19 -0400

    Updated Invaders for class

--- Game-3-Invaders/main.lua
+++ Game-3-Invaders/main.lua
@@ -10,33 +10,80 @@ function love.load(arg)
   --enemy = enemy(100,100)
   bullets = {}
   enemies = {}
+  states = {}
+  states.win = 1
+  states.lose = 2
+  states.playing = 3
+  states.waiting = 4
+
+  state = states.waiting
+
+  score = 0
+end
+
+function love.keypressed(key)
+  if state == states.waiting then
+    state = states.playing
+  end
+  if state == states.lose then
+    state = states.playing
+    score = 0
+    player:reset()
+  end
+  if state == states.win then
+    --put hooks in here to loop back to idle or playing after the win state is reached
+  end
 end
 
 function love.update(dt)
-  spawntimer = spawntimer - dt
-  spawnenemy()
-  player:update(dt)
-  enemy:update(dt)
-  for i, v in ipairs(bullets) do
-    v:update(dt)
-    if v.dead == true then
-      table.remove(bullets, i)
+
+  if state == states.playing then
+    spawntimer = spawntimer - dt
+    spawnenemy()
+    player:update(dt)
+    enemy:update(dt)
+    if player.hp <= 0 then
+      state = states.lose
     end
-    for j,b in ipairs(enemies) do
-      v:checkforcollisions(b)
-      if b.dead == true then
-        table.remove(enemies, j)
+    for i, v in ipairs(bullets) do
+      v:update(dt)
+      if v.y < 0 then
+        v.dead = true
+      end
+      if v.dead == true then
+        table.remove(bullets, i)
+      end
+      for j,b in ipairs(enemies) do
+        v:checkforcollisions(b)
+        if b.dead == true then
+          table.remove(enemies, j)
+          score = score + 1
+        end
+      end
+    end
+    for i,v in ipairs(enemies) do
+      if v.y >= love.graphics.getHeight() then
+        table.remove(enemies, i)
+      end
+    end
+    for i,v in ipairs(enemies) do
+      player:checkforcollisions(v)
+      if v.dead == true then
+        table.remove(enemies,i)
       end
     end
   end
-
 end
 
 function love.draw()
   -- body...
-  player:draw()
-  bullet:draw()
-  enemy:draw()
+  if state == states.playing then
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf("Score: "..score, 20, 20, 50, "center")
+    player:draw()
+    bullet:draw()
+    enemy:draw()
+  end
 end
 
 function spawnenemy()
--- Game-3-Invaders/player.lua
+++ Game-3-Invaders/player.lua
@@ -7,6 +7,8 @@ function player:new()
   self.y = love.graphics.getHeight() - 75
   self.shotcooldown = 0
   self.speed = 500
+  self.hp = 50
+  self.dead = false
 end
 
 function player:update(dt)
@@ -21,11 +23,19 @@ function player:update(dt)
   if love.keyboard.isDown("space") then
     player:shoot(self.x + (self.w/2),self.y)
   end
+
+  if self.hp <= 0 then
+    self.dead = true
+  end
 end
 
 function player:draw()
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+  if self.dead == false then
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+  else
+    --state change
+  end
 end
 
 function player:shoot(x,y)
@@ -33,4 +43,35 @@ function player:shoot(x,y)
     self.shotcooldown = .2
     table.insert(bullets, bullet(x,y))
   end
+end
+
+function player:checkforcollisions(obj)
+  local left = self.x
+  local right = self.x + self.w
+  local top = self.y
+  local bottom = self.y + self.h
+
+  local obj_left = obj.x
+  local obj_right = obj.x + obj.w
+  local obj_top = obj.y
+  local obj_bottom = obj.y + obj.h
+
+  if right > obj_left and
+  left < obj_right and
+  bottom > obj_top and
+  top < obj_bottom then
+    self.hp = self.hp - 10
+    obj.dead = true
+  end
+end
+
+function player:reset()
+  self.w = 50
+  self.h = 50
+  self.x = love.graphics.getWidth() / 2 - self.w/2
+  self.y = love.graphics.getHeight() - 75
+  self.shotcooldown = 0
+  self.speed = 500
+  self.hp = 50
+  self.dead = false
 end
